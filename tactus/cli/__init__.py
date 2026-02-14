c1b914cbf4c848e2cd8e1fceb0f55d44fcf9a961
Ryan Alyn Porter <rap@endymion.com>
2026-02-14 07:27:11 -0500

    Format cli init for black (Tactus-ccc)

--- tactus/cli/__init__.py
+++ tactus/cli/__init__.py
@@ -6,6 +6,7 @@ from __future__ import annotations
 
 from typing import Any
 
+
 def main(*args: Any, **kwargs: Any):
     # Avoid importing tactus.cli.app at package import time.
     # This prevents runpy warnings when running: python -m tactus.cli.app
