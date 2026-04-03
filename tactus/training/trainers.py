51694cce05b1b3ced4ae1311ea290d812dc95361
dereknorrbom <dereknorrbom@gmail.com>
2026-04-03 08:25:40 -0700

    fix(ci): format trainers module for black compliance

--- tactus/training/trainers.py
+++ tactus/training/trainers.py
@@ -13,9 +13,7 @@ from tactus.training.types import CandidateConfig, TrainedModel
 class Trainer(Protocol):
     name: str
 
-    def train(
-        self, candidate: CandidateConfig, data: DatasetBundle, workdir: str
-    ) -> TrainedModel:
+    def train(self, candidate: CandidateConfig, data: DatasetBundle, workdir: str) -> TrainedModel:
         return None
 
 
